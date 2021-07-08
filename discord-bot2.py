671bb41f799a8b9aad15b00c465e18d26f10f7f5
Joel Shine <70741696+JoelShine@users.noreply.github.com>
2021-07-08 19:46:08 +0530

    Add files via upload

---
+++ discord-bot2.py
@@ -0,0 +1,116 @@
+import discord
+from discord.ext import commands, tasks
+from discord.voice_client import VoiceClient
+import youtube_dl
+import sys
+
+from random import choice
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+client = commands.Bot(command_prefix='#')
+
+status = ['Jamming out to music!', 'Study mood !', 'Creative mood !']
+
+@client.event
+async def on_ready():
+    change_status.start()
+    print('Bot is online!')
+
+@client.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name='general')
+    await channel.send(f'Welcome {member.mention}!  This is a nice discord server! See `#help` command for details!')
+
+@client.command(name='hello', help='This command returns a welcome message')
+async def hello(ctx):
+    responses = ['Hola', 'Hello, how are you?', 'Hello', 'Hi']
+    await ctx.send(choice(responses))
+
+@client.command(name='credits', help='This command returns the credits')
+async def credits(ctx):
+    await ctx.send('This is made by \'Joel Shine\' in Python Language !')
+
+@client.command(name='hooray', help='Sound of people applausing!')
+async def hooray(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = await channel.connect()
+    source = FFmpegPCMAudio('hooray.wav')
+    player = voice.play(source)
+
+@client.command(name='play', help='This command plays music')
+async def play(ctx, url):
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+
+    else:
+        channel = ctx.message.author.voice.channel
+
+    await channel.connect()
+
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=client.loop)
+        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('**Now playing:** {}'.format(player.title))
+
+@client.command(name='stop', help='This command stops the music and makes the bot leave the voice channel')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    await voice_client.disconnect()
+
+@client.command(name='bot_quit', help='This command quits the PYBOT!')
+async def bot_quit(ctx):
+    sys.exit()
+
+@tasks.loop(seconds=20)
+async def change_status():
+    await client.change_presence(activity=discord.Game(choice(status)))
+
+client.run("ODYwMzY2MDQxNDk4Nzc5Njg5.YN6MUQ.qJSQ0KEuyoerNwl4S2gR4-J9LtI")
